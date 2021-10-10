36b1b5ec43e75909983fd89a4560345e6731d2aa
christophe <christophegueroult@gmail.com>
2021-10-10 18:17:14 +0200

    actions et todosReducer

--- src/app/core/store/index.ts
+++ src/app/core/store/index.ts
@@ -1,27 +1,15 @@
-import { Action, ActionReducerMap, createReducer } from '@ngrx/store';
-import { Todo } from '../interfaces/todo';
+import { Action, ActionReducerMap } from '@ngrx/store';
+import { todosRecuder, TodoState } from './todos.reducer';
 
-// step 1 decrire l'etat de notre appli via une interface pour le state
 export interface AppState {
-  // tous les elements relatifs aux todo
-  todos: {
-    data: Todo[];
-  };
+  // on peut utiliser maintenant l'interface TodoState
+  todos: TodoState;
 }
 
-// step 2 init state (concerne uniquement la clé data de l'interface AppState)
-export const todoInitialState = {
-  data: [{ message: 'test', done: false }],
-};
-
-// step 3 : create a reducer to modify todos.data
-// createReducer doit prendre au min un param qui est l'état initial de l'appli lié à la clé todos uniquement (data: Todo[])
-export const todosRecuder = createReducer(todoInitialState);
-
-// step 4 : create root reducer. obj qui regroupe tous les reducers de notre application
-// ActionReducerMap est un type générique qui va prendre 2 autres types : le type de notre state global et le type Action
-// c'est cet objet que l'on va passer à StoreModule.forRoot() sur AppModule
+// obj passe au StoreModule.forRoot() sur AppModule
 export const rootReducers: ActionReducerMap<AppState, Action> = {
-  // associer la clé todos du state au todosReducer
   todos: todosRecuder,
 };
+
+// a présent nous avons ici la config de tous nos réducers, l'objet rootReducer qui contient tous nos reducers
+// et l'interface de notre state principal
---
+++ src/app/core/store/todos.reducer.ts
@@ -0,0 +1,59 @@
+import { createReducer, on } from '@ngrx/store';
+import { Todo } from '../interfaces/todo';
+import * as TodosActions from './todos.actions';
+
+// declarer une interface uniquement pour la clé totos du state global
+export interface TodoState {
+  data: Todo[];
+}
+// deplace l'init de la cle data du state global
+export const todoInitialState = {
+  data: [{ message: 'test', done: false }],
+};
+// deplace todosReducer
+// après avoir créé nos action, on les utilise dans todosReducer qui effectuera une modif de TodoState en fonction de l'action
+export const todosRecuder = createReducer(
+  todoInitialState,
+  // on() c'est comme un eventListner qui écoute si une action est dispatch
+  on(
+    TodosActions.addTodoAction,
+    // 2e param est une fn qui va etre appelée si addTotoAction est dispatch
+    // cette fn prend 2 params. Le premier est le state à modifier. le deuxième c'est le payload entier {type: string, item: Todo}
+    // ici on va récupérer en 2e param uniquement la clé item (le type pas besoin) avec la description du typage et cette fn doit tjrs return un nouveau TodoState
+    (state: TodoState, { item }: { item: Todo }): TodoState => {
+      return {
+        ...state, // on destructure pour return un new state cette methode doit etre pure. soit elle return un state inchangé soit, si les state est modifié, elle doit return un new state
+        // jusque là data contient le tableau de Todo[] tel qu'il est dans todoInitialState
+        data: [...state.data, item], // ici on surcharge la clé data qui contient maintenant tous les obj précédents + item qui est l'objet que l'on veut ajouter
+      };
+    }
+  ),
+  on(
+    TodosActions.deleteTodoAction,
+    (state: TodoState, { index }: { index: number }): TodoState => {
+      return {
+        ...state,
+        data: state.data.filter((v, i) => i !== index), // filter return un new tab avec new ref
+      };
+    }
+  ),
+  on(
+    TodosActions.toggleTodoAction,
+    (state: TodoState, { index }: { index: number }): TodoState => {
+      return {
+        ...state,
+        data: state.data.map(
+          (v, i) => (i !== index ? v : { ...v, done: !v.done }) // on surcharge la clé done pour mettre l'inverse de v.done
+        ),
+      };
+    }
+  )
+);
+
+// maintenant, ici on a uniquement tout ce qui concerne la partie todos de notre state
+// on décrit ce qu'il y a dans la clé todos via une interface
+// on a un état initial
+// on a notre reducer
+
+// maintenant qu'on a fini de faire notre reducers qui prend en compte toutes les actions, il faut
+// que dans notre appli, au lieu d'utiliser notre service on dispatch des action, pour cela on va voir les selector
